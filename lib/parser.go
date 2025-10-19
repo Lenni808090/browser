87245bfcbb6251acfe331bdc1fcc0ed774372aef
Lenni <haselenni@gmail.com>
2025-10-19 12:54:57 +0200

    limple parser

--- lib/parser.go
+++ lib/parser.go
@@ -1,5 +1,7 @@
 package lib
 
+import "fmt"
+
 type Parser struct {
 	tokens []Token
 	pos    int
@@ -20,6 +22,13 @@ func (p *Parser) eat() Token {
 	p.pos++
 
 	return token
+}
+
+func (p *Parser) at() Token {
+	if p.pos == len(p.tokens) {
+		return Token{EoF, "EoF"}
+	}
+	return p.tokens[p.pos-1]
 }
 
 func (p *Parser) peek() Token {
@@ -61,17 +70,62 @@ func (p *Parser) Pars() *Node {
 }
 
 func (p *Parser) parseNode() *Node {
-	tkType := p.peek().TokenType
-	switch tkType {
+	tok := p.peek() // look at the next token
+
+	switch tok.TokenType {
 	case OpenTag: // <
 		return p.parseElementNode()
 	case Literal:
 		return p.parseTextNode()
 	default:
-		p.eat()
-		panic("Unkown Node")
+		badTok := p.eat()
+		panic(fmt.Sprintf("Unknown Node: Type=%v, Value=%q", badTok.TokenType, badTok.Value))
 	}
 }
 
+func (p *Parser) parseElementNode() *Node {
+	p.eat() //<
+	tag := p.expect(Identifier, "tag expected").Value
 
-func 
+	var attr []Attr
+	for p.peek().TokenType == Identifier {
+		key := p.eat().Value
+		p.expect(Equals, "equals eypected after value")
+		value := p.expect(Identifier, "value needed after key").Value
+		attr = append(attr, Attr{Key: key, Value: value})
+	}
+	node := NewElementNode(tag, attr)
+
+	if p.peek().TokenType == SelfCloseTag {
+		p.eat()
+		return node
+	}
+
+	p.expect(CloseTag, "> expected")
+
+	for {
+		tok := p.peek()
+		if tok.TokenType == OpenEndTag {
+			p.eat()
+			endTag := p.expect(Identifier, "tag name expected")
+			if endTag.Value != node.Data {
+				panic("tags dont match")
+			}
+			p.expect(CloseTag, "expected > aftzer closing tag")
+			break
+		}
+
+		child := p.parseNode()
+		if child != nil {
+			child.Parent = node
+			node.Children = append(node.Children, child)
+		}
+	}
+
+	return node
+}
+
+func (p *Parser) parseTextNode() *Node {
+	tok := p.eat()
+	return &Node{Type: TextType, Data: tok.Value}
+}
--- main.go
+++ main.go
@@ -5,13 +5,43 @@ import (
 	"fmt"
 )
 
+func printNode(node *lib.Node, depth int) {
+	prefix := ""
+	for i := 0; i < depth; i++ {
+		prefix += "  " // indent
+	}
+
+	switch node.Type {
+	case lib.ElementType:
+		fmt.Printf("%sElement: <%s>\n", prefix, node.Data)
+		for _, attr := range node.Attr {
+			fmt.Printf("%s  Attr: %s=%q\n", prefix, attr.Key, attr.Value)
+		}
+	case lib.TextType:
+		fmt.Printf("%sText: %q\n", prefix, node.Data)
+	case lib.DocumentType:
+		fmt.Printf("%sDocument\n", prefix)
+	}
+
+	for _, child := range node.Children {
+		printNode(child, depth+1)
+	}
+}
+
 func main() {
 	input := `<div class=example>Hello <b>World</b>!</div>`
 
 	lexer := lib.NewLexer(input)
 	tokens := lexer.Tokenize()
 
+	fmt.Println("Tokens:")
 	for _, tok := range tokens {
 		fmt.Printf("Type: %v, Value: %q\n", tok.TokenType, tok.Value)
 	}
+
+	parser := lib.NewParser(tokens)
+	ast := parser.Pars()
+
+	fmt.Println("\nAST:")
+	printNode(ast, 0)
 }
